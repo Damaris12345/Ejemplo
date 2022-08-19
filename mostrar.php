36f75d0ff0427e676bfaa8b63bbe5bd3d38f9d22
Damaris Macario <dammac46@gmail.com>
2022-08-19 13:16:03 -0600

    Version 3

--- mostrar.php
+++ mostrar.php
@@ -1,37 +1,4 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <title>Panel administrativo</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/png" sizes="96x96" href="../assets/images/icono.svg">
-    <script src="../assets/js/sweet-alert.min.js"></script>
-    <link rel="stylesheet" href="../assets/css/sweet-alert.css">
-    <link rel="stylesheet" href="../assets/css/material-design-iconic-font.min.css">
-    <link rel="stylesheet" href="../assets/css/normalize.css">
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/jquery.mCustomScrollbar.css">
-	
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	
-	
-    
-    <script src="../assets/js/modernizr.js"></script>
-    <script src="../assets/js/bootstrap.min.js"></script>
-    <script src="../assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
-    <script src="../assets/js/main.js"></script>
-			<style>
-	 .loading  {
-		 position: fixed;
-		 left: 0px;
-		 top: 0px;
-		 width: 100%;
-		 height: 100%;
-		 z-index: 9999;
-		 background: url('../assets/images/Loading_2.gif') 50% 50% no-repeat rgb(249,249,249);
-		opacity: .8; }
-	</style>
+
 	
 	
 </head>
@@ -481,7 +448,7 @@ $("#imgInp").change(function() { //Cuando el input cambie (se cargue un nuevo ar
   </script>
 	 
 <!--- -----------------------------------------------------------------------------------------------------> 
-<!--	<script>
+	<script>
 	function activo(codalum)
 {
 	var id=codalum;
@@ -516,7 +483,6 @@ $(window).on('load', function () {
      
 });
 </script>
-	 - -----------------------------------------------------------------------------------------------------> 
-
+	 <!--- -----------------------------------------------------------------------------------------------------> 
 </body>
 </html>
