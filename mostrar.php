11b62c5607b4cc7481772160c4ad3e233c014da4
Damaris Macario <dammac46@gmail.com>
2022-08-19 13:28:03 -0600

    Version 4

--- mostrar.php
+++ mostrar.php
@@ -1,4 +1,37 @@
-
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Panel administrativo</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/png" sizes="96x96" href="../assets/images/icono.svg">
+    <script src="../assets/js/sweet-alert.min.js"></script>
+    <link rel="stylesheet" href="../assets/css/sweet-alert.css">
+    <link rel="stylesheet" href="../assets/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="../assets/css/normalize.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/jquery.mCustomScrollbar.css">
+	
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	
+	
+    
+    <script src="../assets/js/modernizr.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="../assets/js/main.js"></script>
+			<style>
+	 .loading  {
+		 position: fixed;
+		 left: 0px;
+		 top: 0px;
+		 width: 100%;
+		 height: 100%;
+		 z-index: 9999;
+		 background: url('../assets/images/Loading_2.gif') 50% 50% no-repeat rgb(249,249,249);
+		opacity: .8; }
+	</style>
 	
 	
 </head>
@@ -31,66 +64,7 @@
 					
 				
 
-    <div class="navbar-lateral full-reset">
-        <div class="visible-xs font-movile-menu mobile-menu-button"></div>
-        <div class="full-reset container-menu-movile nav-lateral-scroll">
-            <div class="logo full-reset all-tittles">
-                <i class="visible-xs zmdi zmdi-close pull-left mobile-menu-button" style="line-height: 55px; cursor: pointer; padding: 0 10px; margin-left: 7px;"></i> 
-                FUNDIPG
-            </div>
-            <div class="nav-lateral-divider full-reset"></div>
-            <div class="full-reset" style="padding: 10px 0; color:#fff;">
-                <figure>
-                    <img src="../assets/images/graduation.svg" alt="Biblioteca" class="img-responsive center-box" style="width:55%;">
-                </figure>
-                <p class="text-center" style="padding-top: 15px;">FUNDIPG</p>
-            </div>
-            <div class="nav-lateral-divider full-reset"></div>
-            <div class="full-reset nav-lateral-list-menu">
-                <ul class="list-unstyled">
-                    <li><a href="../vista/admin/admin_portada.php"><i class="zmdi zmdi-view-dashboard zmdi-hc-fw"></i>&nbsp;&nbsp; Tablero</a></li>
-                    <li><a href="#"><i class="zmdi zmdi-favorite zmdi-hc-fw"></i>&nbsp;&nbsp; Mensajes</a></li>
-                    <li><a href="#"><i class="zmdi zmdi-face zmdi-hc-fw"></i>&nbsp;&nbsp; Académico</a></li>-->
-					<li><a href="asignacion.php"><i class="zmdi zmdi-accounts zmdi-hc-fw"></i>&nbsp;&nbsp; Eventos</a></li>
-					<li><a href="docente.php"><i class="zmdi zmdi-wrench zmdi-hc-fw"></i>&nbsp;&nbsp; Instructores</a></li>
-					<li><a href="alumnos.php"><i class="zmdi zmdi-dns zmdi-hc-fw"></i>&nbsp;&nbsp; Participantes</a></li>
-					<li><a href="../vista/calendario/mostrar.php"><i class="zmdi zmdi-calendar-alt zmdi-hc-fw"></i>&nbsp;&nbsp; Calendario</a></li>
-					<li><a href="../vista/reporte/mostrar.php"><i class="zmdi zmdi-sort-amount-desc zmdi-hc-fw"></i>&nbsp;&nbsp; Reportes</a></li>
-					<li><a href="#"><i class="zmdi zmdi-money-box zmdi-hc-fw"></i>&nbsp;&nbsp; Contabilidad</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <div class="content-page-container full-reset custom-scroll-containers">
-        <nav class="navbar-user-top full-reset">
-            <ul class="list-unstyled full-reset">
-                <figure>
-                   <img src="../assets/images/user01.png" alt="user-picture" class="img-responsive img-circle center-box">
-                </figure>
-                <li style="color:#fff; cursor:default;">
-                    <span class="all-tittles"><?php
-						echo $_SESSION['admin_login'];
-				}
-				?></span>
-                </li>
-                <li  class="tooltips-general exit-system-button" data-href="../vista/cerrar_sesion.php" data-placement="bottom" title="Salir del sistema">
-                    <i class="zmdi zmdi-power"></i>
-                </li>
-                <li  class="tooltips-general search-book-button" data-href="searchbook.html" data-placement="bottom" title="Buscar libro">
-                    <i class="zmdi zmdi-search"></i>
-                </li>
-                <li  class="tooltips-general btn-help" data-placement="bottom" title="Ayuda">
-                    <i class="zmdi zmdi-help-outline zmdi-hc-fw"></i>
-                </li>
-                <li class="mobile-menu-button visible-xs" style="float: left !important;">
-                    <i class="zmdi zmdi-menu"></i>
-                </li>
-                <li class="desktop-menu-button hidden-xs" style="float: left !important;">
-                    <i class="zmdi zmdi-swap"></i>
-                </li>
-            </ul>
-        </nav>
-		
+    
 		
         <div class="container">
             <div class="page-header">
